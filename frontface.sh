634c25e9ece338e1247a08e568971e1c554aafd4
charfweh <affan.ismail2016@gmail.com>
2023-09-12 11:49:29 -0400

    hmm

--- frontface.sh
+++ frontface.sh
@@ -11,14 +11,41 @@ fi
 echo -e "${Green}[+] Running nmap ${Color_Off}\n" 
 
 
-nmap --min-rate 1000 $1 | tail -n +6 | head -n -2 > nmap
+nmap  --min-rate 1000 $1 | tail -n +6 | head -n -2 > nmap
 
+portnumbers=()
+portandservice=()
 while read line
 do
+	#$line | cut -d / -f 1	
 	if [[ $line == *open* ]]
 	then
-		portnumber = $line | $(cut -d / -f 1)
-		echo -e "[+] $portnumber"
+		echo -e "[+] Found port: $line" | cut -d / -f 1
+		#echo -e "$line" | cut -d / -f 1 >> openports
+		portnumbers+=("$(echo "$line" | cut -d / -f 1)")
+		echo -e "$line" | awk -F' ' '{print $3}'
+		service=$(echo "$line" | awk -F' ' '{print $3}')
+		port=$(echo "$line" | cut -d / -f 1)
+		echo -e "PORT $port,$service"
+		portandservice+=(["$port"]="$service")
+		#nmap -sC -sV -oN servicescan $1 -p$portnumber 
 	fi
 
 done < nmap
+echo -e "${portandservice[@]}"
+#for val in "${portnumbers[@]}"
+#do
+#	echo -e "[+] running services and versions enumueration on $val"
+#	nmap -sC -sV $1 -p$val >> servicescan
+#done
+
+for key in "${!portandservice[@]}"
+do 
+	echo "$key => ${portandservice[$key]}"
+	if [[ ${portandservice[$key]} == *http* ]]
+	then
+		echo -e "[+]Running gobuster"
+	fi
+done
+
+
